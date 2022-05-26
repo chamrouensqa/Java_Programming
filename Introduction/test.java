f60317358755c7173829b90888120146ab1969fe
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-26 09:51:21 +0700

    Using a comment after code

--- Introduction/test.java
+++ Introduction/test.java
@@ -5,6 +5,11 @@ public class test{
         System.out.println("Multiple println");
         System.out.println(3+7);
         System.out.print("Not insert new line");
-        System.out.print("Test about not insert new line");
+        System.out.println("Test about not insert new line");
+        System.out.println("--------------------------------------");
+        //Single command test
+        System.out.println("Here above is the single comment");
+        System.out.println("--------------------------------------");
+        System.out.println("Using a comment after code");//This the commenter after code
     }
 }
