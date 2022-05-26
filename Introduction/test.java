8a443b39eeb4ffecb9dc1e4e7418dfb80d55edfa
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-26 09:24:16 +0700

    Adding javac

--- Introduction/test.java
+++ Introduction/test.java
@@ -1,5 +1,5 @@
-public class test {
+public class test{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.printf("Hello world!");
     }
 }
