f58c6562c26c875f7861215d4a5ffc590f4a1dea
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2022-05-26 09:44:56 +0700

    perform math function

--- Introduction/test.java
+++ Introduction/test.java
@@ -3,5 +3,6 @@ public class test{
         System.out.println("Hello world!");
         System.out.println("Multiple output");
         System.out.println("Multiple println");
+        System.out.println(3+7);
     }
 }
